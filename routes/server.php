9e04632f17e4d15a153eb714d2dca1250ef606b3
Gajendra Singh <work.gajendra@gmail.com>
2026-01-03 01:30:59 +0530

    Update server.php

--- routes/server.php
+++ routes/server.php
@@ -1,7 +1,3 @@
 <?php
-    // $base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
-    // $base .= '://'.$_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-    // $base = 'https://casbara.himachalsociety.com/';
-    $base = request()->getSchemeAndHttpHost();
-
+    $base = rtrim(config('app.url'), '/') . '/';
 ?>
